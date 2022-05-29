f30618ee7b4efb619e42d1de1b2e0d58e4009066
mehunk <mehunk@163.com>
2022-05-29 08:11:54 +0800

    feat: 将默认导出修改为 Promise

--- api/index.ts
+++ api/index.ts
@@ -1,22 +1,26 @@
 import express from 'express';
 
-const app = express();
+async function getApp () {
+  const app = express();
 
-app.get('/', (req, res) => {
-  const path = `/item/${Date.now()}`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-});
+  app.get('/', (req, res) => {
+    const path = `/item/${Date.now()}`;
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+    res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+  });
 
-app.get('/item/:slug', (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
-});
+  app.get('/item/:slug', (req, res) => {
+    const { slug } = req.params;
+    res.end(`Item: ${slug}`);
+  });
 
-app.delete('/item/:slug', (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug} has been deleted!`);
-});
+  app.delete('/item/:slug', (req, res) => {
+    const { slug } = req.params;
+    res.end(`Item: ${slug} has been deleted!`);
+  });
 
-export default app;
+  return app
+}
+
+export default getApp();
